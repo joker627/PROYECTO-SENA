9948ac81acadfbc7c6ac9a966763320b12ea097d
Manuel-Dev <73049543+joker627@users.noreply.github.com>
2025-10-09 23:34:06 -0500

    Delete backend/config/conexion.py

--- backend/config/conexion.py
+++
@@ -1,9 +0,0 @@
-# ===== CONFIGURACIÓN PARA DB=====
-# detecta estos nombres y los reemplaza
-import os
-MYSQLHOST = os.environ['MYSQLHOST'] 
-MYSQLUSER = os.environ['MYSQLUSER']
-MYSQLPASSWORD = os.environ['MYSQLPASSWORD']
-MYSQLDATABASE = os.environ['MYSQLDATABASE']
-MYSQLPORT = int(os.environ['MYSQLPORT'])
-MYSQLCHARSET = 'utf8mb4'
