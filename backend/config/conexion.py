7f68b4f0328776c578de97d4a7416dda0f61dabb
Manuel-Dev <73049543+joker627@users.noreply.github.com>
2025-10-09 23:43:07 -0500

    Create conexion.py for MySQL connection settings
    Add configuration variables for MySQL connection

---
+++ backend/config/conexion.py
@@ -0,0 +1,8 @@
+# ===== CONFIGURACIÓN PARA LA CONEXION =====
+#inyecta automáticamente estos valores
+MYSQLHOST = None 
+MYSQLUSER = None  
+MYSQLPASSWORD = None  
+MYSQLDATABASE = None  
+MYSQLPORT = None  
+MYSQLCHARSET = 'utf8mb4'
